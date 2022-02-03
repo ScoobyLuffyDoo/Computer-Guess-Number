a73bc1cbe155dc694dbce437c1f83d37c93a9783
ScoobyLuffyDoo <ScoobyLuffyDoo@gmail.com>
2022-02-04 01:33:00 +0200

    last 2 diget number filter

--- index2.py
+++ index2.py
@@ -5,39 +5,54 @@ import numpy as np
 
 class guessNumber:
     
-    def __init__(self,i_numberArray,i_key):
+    def __init__(self,i_numberArray,i_key,i_median):
        self.i_numberArray = i_numberArray
        self.i_key = i_key
+       self.i_median = i_median
 
     def numberFilter(self):
         if self.i_key.upper() == 'Y':
-            w_median = np.median(self.i_numberArray)
-            o_numberArray = np.delete(self.i_numberArray,np.where(self.i_numberArray<=round(w_median)))           
-            question = f"Is Your Number Greater then {w_median}"
+            print(self.i_numberArray)
+            o_numberArray = np.delete(self.i_numberArray,np.where(self.i_numberArray<=round(self.i_median)))       
+            new_median = round(np.median(o_numberArray))
+            question = f"Is Your Number Greater then {new_median}"
             haveNumber = False
-            return o_numberArray ,question, haveNumber
+            print(o_numberArray)
+            return (o_numberArray,question,haveNumber,new_median)
             
-        elif self.i_key.upper() == 'N': 
-            w_median = np.median(self.i_numberArray)
-            o_numberArray = np.delete(self.i_numberArray,np.where(self.i_numberArray>=round(w_median)))     
-            question = f"Is Your Number Greater then {w_median}"
-            haveNumber = False
-            return o_numberArray ,question, haveNumber
+        elif self.i_key.upper() == 'N':  
+            if len(self.i_numberArray) > 2:
+                o_numberArray = np.delete(self.i_numberArray,np.where(self.i_numberArray>round(self.i_median)))   
+                new_median = round(np.median(o_numberArray))
+                question = f"Is Your Number Greater then {new_median}"
+                haveNumber = False
+                print(o_numberArray)
+            elif len(self.i_numberArray)<=2:
+                # Write new logic for last 2 numbers
+                # new_median = round(np.median(self.i_numberArray))
+                # o_numberArray = np.delete(self.i_numberArray,np.where(self.i_numberArray>=round(new_median)))                      
+                # question = f"Is Your Number Greater then {o_numberArray}"
+                # new_median = 1
+                # haveNumber = False
+            return (o_numberArray,question,haveNumber,new_median)
         else:
-            question = "They Key is Lower"   
+            new_median = np.median(self.i_numberArray)
+            question = f"Is Your Number Greater then {new_median}"   
             o_numberArray = self.i_numberArray
             haveNumber = False
-            return (o_numberArray ,question, haveNumber)
+            return (o_numberArray,question,haveNumber,new_median)
          
     
 
 
 w_numberGuess_arr  = np.arange(101)
 haveNumber =False
-yes_no = 'Y'
+yes_no = ' '
+w_median =0
 while haveNumber == False:
     
-    tst = guessNumber(w_numberGuess_arr,yes_no).numberFilter() 
+    tst = guessNumber(w_numberGuess_arr,yes_no,w_median).numberFilter() 
+    w_median = tst[3]
     w_numberGuess_arr =tst[0]
     haveNumber =tst[2]
     yes_no = input(f'{tst[1]}\n')   
